4c8d071a77bcc9cab3b4662788083ac8feabfa6c
unknown <yliastupac25@gmail.com>
2024-10-08 16:54:29 +0300

    section - personal information

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,16 @@
-  
+$(document).ready(function() {
+	$('select').niceSelect();
+	$('[type="tel"]').mask('+38(000)-000-00-00');
+	// $( '[type="date"]' ).datepicker({
+	// 	buttonImage: "../img/icon-calendar.svg",
+	// 	showWeek: false
+	// });
+
+	$('[data-calendar]').wrap('<div class="calendar-container"></div>');
+
+	$( '[data-calendar]' ).datepicker({
+		showOn: "button",
+		buttonImage: "assets/img/icon-calendar.svg",
+		buttonImageOnly: true,
+	});
+})
