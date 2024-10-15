376d120c263bbd2132c585b3e371d4a89b90d6e4
unknown <yliastupac25@gmail.com>
2024-10-15 16:19:10 +0300

    finished form

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -13,4 +13,39 @@ $(document).ready(function() {
 
 })
 
+document.addEventListener('input', function(event) {
+	if (event.target.type === 'number') {
+	event.target.value = event.target.value.replace(/[^\d.]/g, '');
+	}
+});
+
+
+let x = 10
+
+const msg1 = "x==1"
+const msg2 = "x==2"
+const msg3 = "x==3"
+const msg4 = "undefined"
+
+function write_1 () {
+	console.log(msg1)
+}
+
+function write_2 () {
+	console.log(msg2)
+}
+
+function write_3 () {
+	console.log(msg3)
+}
+
+if (x == 1) {
+	write_1 ();
+} else if (x == 2) {
+	write_2 ();
+} else if (x == 3) {
+	write_3 ()
+} else {
+	console.log(msg4)
+}
 
